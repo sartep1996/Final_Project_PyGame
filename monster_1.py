eae6bf3fa471bca01f308a4b79902b2f4ca1e715
Petras Anskaitis <sartep1996@gmail.com>
2023-06-29 10:02:32 +0300

    fixed monster animation

--- monster_1.py
+++ monster_1.py
@@ -39,7 +39,7 @@ class Monster1(pg.sprite.Sprite):
         self.monster_rect.topleft = position #sets initial position
         self.is_monster_image = True
         self.animation_timer = 0
-        self.animation_delay = 700
+        self.animation_delay = 350
         self.movement_speed = 5
         self.last_moved = ''
         self.last_frame_time = time.time()
@@ -69,30 +69,20 @@ class Monster1(pg.sprite.Sprite):
             self.direction = 1
 
             
-
-
    
     def draw_monster(self, screen):
         if self.last_moved == BOUNDRY_LEFT:
-            if self.is_monster_image:
-                screen.blit(self.monster_image_left_still, self.monster_rect)
-            elif self.animation_timer >= self.animation_delay :
+            if self.direction != 0 and not self.is_monster_image:
                 screen.blit(self.monster_image_left_2, self.monster_rect)
             else:
                 screen.blit(self.monster_image_left_1, self.monster_rect)
-
         elif self.last_moved == BOUNDRY_RIGHT:
-            if self.is_monster_image:
-                screen.blit(self.monster_image_right_still, self.monster_rect)
-            elif self.animation_timer >= self.animation_delay:
+            if self.direction != 0 and not self.is_monster_image:
                 screen.blit(self.monster_image_right_2, self.monster_rect)
             else:
                 screen.blit(self.monster_image_right_1, self.monster_rect)
-
-        elif self.is_monster_image:
-                screen.blit(self.monster_image_down_still, self.monster_rect)
-
-    
+        else:
+            screen.blit(self.monster_image_down_still, self.monster_rect)
 
 
 
@@ -105,28 +95,11 @@ class Monster1(pg.sprite.Sprite):
         if self.direction != 0:
             self.animation_timer += delta_time * 700
             if self.animation_timer >= self.animation_delay:
-                self.animation_timer = 350
-                self.is_monster_image = True
-            else:
-                self.is_monster_image = False
-
-  
-
-
-
-        # if not self.is_monster_image and self.last_moved != 0:
-        #     self.animation_timer += delta_time * 700
-        #     if self.animation_timer >= self.animation_delay:
-        #         self.animation_timer = 0
-        #         self.is_monster_image = True
-
-
-
-
-
-
-
-
+                self.animation_timer = 0  # Reset the timer
+                self.is_monster_image = not self.is_monster_image  # Toggle the image
+        else:
+            self.is_monster_image = True
+            self.animation_timer = 0  # Reset the timer
 
 monster_image_path_raw_down = 'Images/Monster_1_sprites/monster_down_still.png'
 monster_image_down_path_1 = 'Images/Monster_1_sprites/monster_down_1.png'
